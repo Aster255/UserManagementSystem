0d8c0d7340cac702acd011a6339b5aa47c5235f6
Paul Adrian Alcos <paaalcos@gmail.com>
2024-05-12 00:48:54 +0800

    api for auth register login and logout

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Credentials do not Match');
+        }
+
+        $user = User::where('email', $request->email)
+            ->whereNull('deleted_at')
+            ->first();
+
+        $user->tokens()->delete();
+
+        $token = $user->createToken("generateApiKey")->plainTextToken;
+
+        return $this->success([
+            'user' => $user,
+            'token' => $token
+        ], "login successful");
+    }
+
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken("generateApiKey")->plainTextToken
+        ], "registration successful");
+    }
+
+
+    public function logout()
+    {
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->success([
+            'message' => 'user logged out'
+        ]);
+    }
+}
